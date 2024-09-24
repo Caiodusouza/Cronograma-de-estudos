50043e96d843a0084f7bff8efe02971ca4d285aa
Caio Eduardo De Souza <127906616+Caiodusouza@users.noreply.github.com>
2024-09-24 10:22:00 -0300

    Primeiro Commit do projeto

---
+++ Estudo.cs
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct material {
+    string nome;
+    int horas;
+};
+
+class cronogramasEstudos {
+    private :
+         vector<Material> materias;
+
+    public :
+        void adicionarMateria(cronogramasEstudos string& nome, int horas) {
+            materia novaMateria = { nome,horas};
+            ma
+        }
+}
