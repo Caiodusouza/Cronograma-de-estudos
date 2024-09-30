51de096a3ab7e5ad9c49af7d3fdd316a6a425321
Caio Eduardo De Souza <127906616+Caiodusouza@users.noreply.github.com>
2024-09-30 08:38:14 -0300

    Update Cronograma.cpp

--- Cronograma.cpp
+++ Cronograma.cpp
@@ -22,8 +22,31 @@ public:
 
     void listarMaterias() {
         cout << "\nCronograma de Estudos:\n";
-        for (const auto& materia : materias) {
-            cout << "Materia: " << materia.nome << ", Horas: " << materia.horas << endl;
+        if (materias.empty()) {
+            cout << "Nenhuma materia cadastrada.\n";
+            return;
+        }
+        for (size_t i = 0; i < materias.size(); ++i) {
+            cout << i + 1 << ". Materia: " << materias[i].nome << ", Horas: " << materias[i].horas << endl;
+        }
+    }
+
+    void removerMateria(int indice) {
+        if (indice >= 0 && indice < materias.size()) {
+            cout << "Materia removida: " << materias[indice].nome << endl;
+            materias.erase(materias.begin() + indice);
+        } else {
+            cout << "Indice invalido!\n";
+        }
+    }
+
+    void editarMateria(int indice, const string& novoNome, int novasHoras) {
+        if (indice >= 0 && indice < materias.size()) {
+            materias[indice].nome = novoNome;
+            materias[indice].horas = novasHoras;
+            cout << "Materia editada com sucesso.\n";
+        } else {
+            cout << "Indice invalido!\n";
         }
     }
 };
@@ -38,28 +61,58 @@ int main() {
         cout << "\nMenu:\n";
         cout << "1. Adicionar materia\n";
         cout << "2. Listar materias\n";
-        cout << "3. Sair\n";
+        cout << "3. Remover materia\n";
+        cout << "4. Editar materia\n";
+        cout << "5. Sair\n";
         cout << "Escolha uma opcao: ";
         cin >> opcao;
 
         switch (opcao) {
             case 1:
                 cout << "Digite o nome da materia: ";
-                cin >> nome;
+                cin.ignore(); // Limpa o buffer
+                getline(cin, nome);
                 cout << "Digite o numero de horas de estudo: ";
                 cin >> horas;
-                cronograma.adicionarMateria(nome, horas);
+                if (horas > 0) {
+                    cronograma.adicionarMateria(nome, horas);
+                } else {
+                    cout << "O numero de horas deve ser positivo.\n";
+                }
                 break;
             case 2:
                 cronograma.listarMaterias();
                 break;
-            case 3:
+            case 3: {
+                cout << "Digite o numero da materia a ser removida: ";
+                int indice;
+                cin >> indice;
+                cronograma.removerMateria(indice - 1);
+                break;
+            }
+            case 4: {
+                cout << "Digite o numero da materia a ser editada: ";
+                int indice;
+                cin >> indice;
+                cout << "Digite o novo nome da materia: ";
+                cin.ignore(); // Limpa o buffer
+                getline(cin, nome);
+                cout << "Digite o novo numero de horas de estudo: ";
+                cin >> horas;
+                if (horas > 0) {
+                    cronograma.editarMateria(indice - 1, nome, horas);
+                } else {
+                    cout << "O numero de horas deve ser positivo.\n";
+                }
+                break;
+            }
+            case 5:
                 cout << "Saindo do programa...\n";
                 break;
             default:
                 cout << "Opcao invalida. Tente novamente.\n";
         }
-    } while (opcao != 3);
+    } while (opcao != 5);
 
-    return 0;
+    return 0;
 }
